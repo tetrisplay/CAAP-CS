4b66f98ba775edefcdf6b8be0fabb9983b66a3da
ctetrick <ctetrick@uchicago.edu>
2018-07-12 15:31:34 -0500

    Homework1 Fibonnaci complete

---
+++ hw1/Feet_Converter.py
@@ -0,0 +1,9 @@
+#This program converts feet to yards
+def feet_converter():
+    print("Hi! This program converts feet to yards.")
+    f = eval(input("Please input your feet value: "))
+    y = (f/3)
+    print("Your input in yards is equal to", y)
+
+feet_converter()
+                
