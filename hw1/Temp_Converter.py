71a3841d1b21292836d2a423abd425e3a4bc6db6
ctetrick <ctetrick@uchicago.edu>
2018-07-12 15:32:40 -0500

    Some flaws in converter, eventually correct

---
+++ hw1/Temp_Converter.py
@@ -0,0 +1,6 @@
+def Temp_Converter():
+    celsius = input("What is the tempurature in celsius")
+    fahrenheit = ((9/5 * celsius) + 32)
+    print("The tempurature is", fahrenheit, "degrees fahrenheit.")
+
+Temp_Converter()
