5da3bb48121ad060f68cdce74f2f9c19839f3f22
ctetrick <ctetrick@uchicago.edu>
2018-07-18 13:58:57 -0500

    adding lab2 resources

---
+++ Lab2/game/make.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# For Mac Users, to make the exe file ofr the game.
+pyinstaller --onefile game.py --name mygame
--- Lab2/lab2_resources/game/make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# For Mac Users, to make the exe file ofr the game.
-pyinstaller --onefile game.py --name mygame
