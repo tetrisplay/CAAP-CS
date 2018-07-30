7e99397d468941bd1e38388d11c14bc463bb5b61
ctetrick <ctetrick@uchicago.edu>
2018-07-29 22:53:41 -0500

    Update

--- Lab3/my_game.py
+++ Lab3/my_game.py
@@ -5,71 +5,80 @@ from random import randint
 
 # global variable of chutes and ladders
 # remenber to let your function know you're using this variable with 'global'
-chutes_ladders = {4 : 7, 8 : 15, 12 : 2, 14: 6}
+chutes_ladders = {4 : 7,
+					8 : 15,
+					12 : 2,
+					14 : 6}
+
 
 # Rolls a die of six sides and returns the result
 def roll_die():
-    return random.randint((1,6))
-         
-# makes a game (just a list) of the given dimensions
-def makeGame(x, y):
-    list = []
-    for i in range((x*y)):
-        list.append(i+1)
-    print(list)
+    return randint(1,6)	
 
+# makes a game (just a list) of the given dimensions
+def makeGame(w, l):
+    game_board = []
+    for i in range((w*l)):
+        game_board.append(i+1)
+    print(game_board)
+	
 # checks if given square is a chutes or ladders
 def is_chutes_ladders(d):
     global chutes_ladders
     for i in chutes_ladders:
         if i == d:
             return True
-    return False
-      
-
+        return False      
 # function to make and play a game
-#Collaborated w/ Kevin Song
-def play_game():
-    option = input("What mode would you like to play in? 1 for player, 2 for computer: ")
 
-    if option == 1:
+def play_game():
+    mode = input("What mode would you like to play in? 'p' for player, 'c' for computer automated: ")
+        
+    if mode == 'p':
+        players = int(input("How many players?: "))
         state = []
-        global p
-        p = int(input("How many people are playing?"))
-    for i in range (p):
-        playdiction.append({"state":1, "moves":0})
-    makeGame(6,6)
-    
-    print(state)
-    gameover = False
-    while gameover == False:
-        for player in range(p):
-            print("Player", player+1, "is up next.")
-            roll = input("Press any key to roll")
-            die = roll_die()
-            print("Player",player+1,"you rolled a",roll_die())
-            state[player]['position'] += die
-            state[player]['moves'] += 1
-            if state[player]['position'] in chutes_ladders:
-                state[player]['position'] = chutes_ladders[state[player]['position']]
-            if state[player]['position'] > (6*6):
-                gameover == True
-                state[player]['position'] == 6*6
-                print("player " + str(player +1) + ": you won!")
-                print(state)
-                break
-    if option == 2:
-        makeGame(6,6)
+        for i in range(players):
+             state.append({'position':1,'moves':0})
+
+        w = int(input("Enter a width: "))
+        l = int(input("Enter a length: "))
+        makeGame(w, l)
+
+
+        print(state)
+        gameover = False
+        while gameover == False:
+            for player in range(players):
+                print("Player",player+1,"it is your turn")
+                roll = input("Press any letter to roll the die: ")
+                die = roll_die()
+                print("Player",player+1,"you rolled a",roll_die())
+                state[player]['position'] += die
+                state[player]['moves'] += 1
+                if state[player]['position'] in chutes_ladders:
+                    state[player]['position'] = chutes_ladders[state[player]['position']]
+                if state[player]['position'] > (w*l):
+                    gameover == True
+                    state[player]['position'] == w*l
+                    print("player " + str(player +1) + ": you won!")
+                    print(state)
+                    break
+
+
+    if mode == 'c':
+        w = int(input("Enter a width: "))
+        l = int(input("Enter a length: "))
+        makeGame(w, l)
         position = 1
         moves = 0
-        while position < (6*6):
+        while position < (w*l):
             position += roll_die()
             moves += 1
             if position in chutes_ladders:
                 position = chutes_ladders[position]
-            if position == (6*6):
+            if position == (w*l):
                 break
-            print(moves,"moves")
+        print(moves,"moves")
 
 play_game()
 
@@ -80,15 +89,14 @@ def simulate_game(rseed=None):
     rand = Random(rseed)
     position = 1
     turns = 0
-    while position < 36:
+    while position < w*l:
         turns += 1
         roll = rand.randint(1,6)
         
-        if position + roll > 36:
+        if position + roll > w*l:
             continue
             
         position += roll
-        
-       
+           
         position = chutes_ladders.get(position, position)
     return turns
