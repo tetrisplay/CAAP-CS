8998f57c84e58274bb7cbe053b263dbbde10a074
reyesj5 <reyesj5@uchicago.edu>
2018-08-08 15:24:13 -0500

    adding grades for assignment_01 and adding total grade file

--- Lab2/game/game.py
+++ Lab2/game/game.py
@@ -18,7 +18,9 @@ def game_over(won):
     global name
     global moves
     score = Score(name, moves)
+    # ***************************************************
     # FIX You should only update if the won (goes inside the if statement)
+    # *************************************************
     leaderboard.update(score)
     if (won):
         print("\nYou Won!") 
